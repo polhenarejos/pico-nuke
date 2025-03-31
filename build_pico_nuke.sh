49b5bddc99082abd90d77eee6883e25fbbedee29
IsayIsee <solin.zhan@gmail.com>
2025-03-31 13:59:39 +0800

    Update build_pico_nuke.sh
    fix may be delete all in pico-nuke project

--- build_pico_nuke.sh
+++ build_pico_nuke.sh
@@ -3,10 +3,16 @@
 VERSION_MAJOR="1"
 VERSION_MINOR="4"
 
-rm -rf release/*
-cd build_release
-mkdir -p release
-cd build_release
+if [ ! -d "build_release" ]; then
+  mkdir build_release
+fi
+if [ ! -d "release" ]; then
+    mkdir release
+fi
+cd release
+rm -rf *
+cd ../build_release
+
 PICO_SDK_PATH="${PICO_SDK_PATH:-../../pico-sdk}"
 board_dir=${PICO_SDK_PATH}/src/boards/include/boards
 for board in "$board_dir"/*
